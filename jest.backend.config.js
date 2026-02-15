0199568755964579863c81edaa9cf78b98f58e83
Alek Kwek <40349610+AlekKwek18@users.noreply.github.com>
2026-02-16 01:13:22 +0800

    update backend config jest js

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -10,7 +10,7 @@ export default {
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["config/**", "controllers/categoryController.js"],
+  collectCoverageFrom: ["config/**", "controllers/categoryController.js", "controllers/productController.js"],
   coverageThreshold: {
     global: {
       lines: 100,
