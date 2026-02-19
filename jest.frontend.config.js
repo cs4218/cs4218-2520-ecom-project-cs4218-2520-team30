b582b42a577d5e6548f397bf3aa213ca7754c2ea
stephaneleong <stephaneleong@gmail.com>
2026-02-19 13:15:57 +0800

    Add Profile files to test frontend config

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -25,12 +25,13 @@ export default {
     "<rootDir>/client/src/pages/Search.test.js",
     "<rootDir>/client/src/pages/HomePage.test.js",
     "<rootDir>/client/src/pages/Categories.test.js",
-    "<rootDir>/client/src/hooks/useCategory.test.js"
+    "<rootDir>/client/src/hooks/useCategory.test.js",
     "<rootDir>/client/src/pages/About.test.js",
     "<rootDir>/client/src/pages/Pagenotfound.test.js",
     "<rootDir>/client/src/components/Form/SearchInput.test.js",
     "<rootDir>/client/src/components/*.test.js",
-    "<rootDir>/client/src/context/search.test.js"
+    "<rootDir>/client/src/context/search.test.js",
+    "<rootDir>/client/src/pages/user/*.test.js",
   ],
 
   // jest code coverage
@@ -41,14 +42,15 @@ export default {
     "client/src/pages/Categories.js",
     "client/src/components/Form/SearchInput.js",
     "client/src/context/search.js",
-    "client/src/hooks/useCategory.js"
+    "client/src/hooks/useCategory.js",
     "client/src/pages/HomePage.js",
     "client/src/pages/About.js",
     "client/src/pages/Pagenotfound.js",
     "client/src/components/Header.js",
     "client/src/components/Footer.js",
     "client/src/components/Layout.js",
-    "client/src/components/Spinner.js"
+    "client/src/components/Spinner.js",
+    "client/src/pages/user/**",
   ],
   coverageThreshold: {
     global: {
