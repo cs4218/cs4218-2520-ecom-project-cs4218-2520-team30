6d0230209f549c0a67eba081c1f7a4bb83531382
stephaneleong <stephaneleong@gmail.com>
2026-02-21 18:45:52 +0800

    Add test cases for UserMenu.js

---
+++ client/src/components/UserMenu.test.js
@@ -0,0 +1,46 @@
+// Leong Soon Mun Stephane, A0273409B
+import { jest, describe, beforeEach, it, expect } from "@jest/globals";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from "react-router-dom";
+import UserMenu from "./UserMenu";
+
+describe('UserMenu Component', () => { // Leong Soon Mun Stephane, A0273409B
+        beforeEach(() => {
+            jest.clearAllMocks();
+        });
+
+    it('should render all text elements if successful', () => { // Leong Soon Mun Stephane, A0273409B
+        // Arrange
+
+        // Act
+        render(
+            <MemoryRouter>
+                <UserMenu/>
+            </MemoryRouter>
+        )
+
+        // Assert
+        expect(screen.getByText('Dashboard')).toBeInTheDocument();
+        expect(screen.getByText('Profile')).toBeInTheDocument();
+        expect(screen.getByText('Orders')).toBeInTheDocument();
+    })
+
+    it('should render navLinks if successful', () => { // Leong Soon Mun Stephane, A0273409B
+        // Arrange
+
+        // Act
+        render(
+            <MemoryRouter>
+                <UserMenu />
+            </MemoryRouter>
+        )
+
+        // Assert
+        let profileLink = screen.getByRole('link', { name: /profile/i });
+        expect(profileLink).toHaveAttribute('href', '/dashboard/user/profile');
+        let ordersLink = screen.getByRole('link', { name: /profile/i });
+        expect(ordersLink).toHaveAttribute('href', '/dashboard/user/profile');       
+    })
+})
