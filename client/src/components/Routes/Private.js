a0414b9787169c43a7e6bb03938c688b902f4661
stephaneleong <stephaneleong@gmail.com>
2026-02-22 22:52:06 +0800

    Remove import

--- client/src/components/Routes/Private.js
+++ client/src/components/Routes/Private.js
@@ -2,7 +2,7 @@ import { useState,useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
 import axios from 'axios';
-// import { set } from "mongoose"; // Leong Soon Mun Stephane, A0273409B
+// removed set import from "mongoose" Leong Soon Mun Stephane, A0273409B
 import Spinner from "../Spinner";
 
 export default function PrivateRoute(){
