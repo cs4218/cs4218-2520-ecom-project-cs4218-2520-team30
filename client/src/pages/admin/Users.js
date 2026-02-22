4556e3b2f7614a46cc464924848d60a02d4efa76
stephaneleong <stephaneleong@gmail.com>
2026-02-22 15:12:30 +0800

    Fix formatting

--- client/src/pages/admin/Users.js
+++ client/src/pages/admin/Users.js
@@ -22,12 +22,12 @@ const Users = () => {
   }, []);
   return (
     <Layout title={"Dashboard - All Users"}>
-        <div className="container-fluid m-3 p-3">
-       <div className="row">
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
           <div className="col-md-3">
             <AdminMenu />
-           </div>
-           <div className="col-md-9">
+          </div>
+          <div className="col-md-9">
             <h1>All Users</h1>
             <div className="table-responsive border shadow">
               <table className="table">
@@ -53,9 +53,9 @@ const Users = () => {
                 </tbody>
               </table>
             </div>
-            </div>
+          </div>
         </div>
-        </div> 
+      </div>
     </Layout>
   );
 };
